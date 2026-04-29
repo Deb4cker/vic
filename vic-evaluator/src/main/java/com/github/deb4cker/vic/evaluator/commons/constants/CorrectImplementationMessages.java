e5c2db432e4158ad7f209707039d1028f95dd9eb
Nicolas <ndebacher@gmail.com>
2026-04-28 23:24:40 -0300

    hotfix: Code review

--- vic-evaluator/src/main/java/com/github/deb4cker/vic/evaluator/commons/constants/CorrectImplementationMessages.java
+++ vic-evaluator/src/main/java/com/github/deb4cker/vic/evaluator/commons/constants/CorrectImplementationMessages.java
@@ -4,7 +4,6 @@ public class CorrectImplementationMessages {
     public static final String CORRECT_IMPLEMENTATION_OF_ATTRIBUTE = "Atributo %s";
     public static final String CORRECT_IMPLEMENTATION_OF_METHOD = "Método %s";
     public static final String CORRECT_IMPLEMENTATION_OF_CONSTRUCTOR = "Construtor implementado";
-
     public static final String CORRECT_IMPLEMENTATION_OF_CLASS = "Classe %s corretamente implementada! 🎉";
     public static final String CORRECT_IMPLEMENTATION_OF_RELATIONSHIPS = "Todos os relacionamentos corretamente implementados! 🎉";
 
